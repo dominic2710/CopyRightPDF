49d5eb6fbebb4c72d1ee328eaa34a5baebd6816f
dominic2710 <61504491+dominic2710@users.noreply.github.com>
2023-03-29 22:39:27 +0700

    Add mobile project

---
+++ CopyrightPDF.Mobile/CopyrightPDF.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CopyrightPDF.Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ CopyrightPDF.Mobile/CopyrightPDF.Mobile/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CopyrightPDF.Mobile;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void ContentPage_Loaded(System.Object sender, System.EventArgs e)
+    {
+		
+    }
+}
+
+
